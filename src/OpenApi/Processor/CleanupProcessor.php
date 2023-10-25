cce59e80aaec301cbe6bb24566e1ef5bc3f92f05
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-10-25 13:36:52 -0400

    allow to override default global responses

--- src/OpenApi/Processor/CleanupProcessor.php
+++ src/OpenApi/Processor/CleanupProcessor.php
@@ -15,6 +15,31 @@ readonly class CleanupProcessor implements ProcessorInterface
             return;
         }
 
+        $this->removeDuplicatedResponses($analysis);
+        $this->removeUselessSchemas($analysis);
+    }
+
+    public static function priority(): int
+    {
+        return -100;
+    }
+
+    protected function removeDuplicatedResponses(Analysis $analysis): void
+    {
+        foreach ($analysis->openapi->components->responses as $i => $response) {
+            if (!isset($responses[$response->response])) {
+                $responses[$response->response] = true;
+
+                continue;
+            }
+
+            unset($analysis->openapi->components->responses[$i]);
+            $this->detachAnnotationRecursively($response, $analysis);
+        }
+    }
+
+    protected function removeUselessSchemas(Analysis $analysis): void
+    {
         foreach ($analysis->openapi->components->schemas as $i => $schema) {
             foreach ($analysis->annotations as $annotation) {
                 if (property_exists($annotation, 'ref') && (string) $annotation->ref === '#/components/schemas/'.$schema->schema) {
@@ -25,11 +50,6 @@ readonly class CleanupProcessor implements ProcessorInterface
             $this->detachAnnotationRecursively($schema, $analysis);
             unset($analysis->openapi->components->schemas[$i]);
         }
-    }
-
-    public static function priority(): int
-    {
-        return -100;
     }
 
     protected function detachAnnotationRecursively($annotation, Analysis $analysis): void
