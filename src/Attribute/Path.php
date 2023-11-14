e2e537c2170c129036f4d1fffa88cc30978d06e7
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-11-13 19:48:52 -0500

    improve Path attribute extensibility

--- src/Attribute/Path.php
+++ src/Attribute/Path.php
@@ -12,6 +12,9 @@ use OpenApi\Generator;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
 class Path extends PathParameter
 {
+    /**
+     * @param array|class-string|null $enum
+     */
     public function __construct(
         // path properties
         ?string $parameter = null,
@@ -23,7 +26,7 @@ class Path extends PathParameter
         ?bool $allowEmptyValue = null,
         object|string|null $ref = null,
         ?Schema $schema = null,
-        mixed $example = Generator::UNDEFINED,
+        mixed $example = null,
         ?array $examples = null,
         JsonContent|array|Attachable|XmlContent|null $content = null,
         ?string $style = null,
@@ -34,29 +37,64 @@ class Path extends PathParameter
         ?array $x = null,
         ?array $attachables = null,
         // custom properties
-        public readonly string $format = Generator::UNDEFINED,
-        public readonly array|string $enum = Generator::UNDEFINED,
+        public ?string $format = null,
+        public array|string|null $enum = null,
     ) {
+        $defaults = $this->defaults();
+
+        $this->format = $format ?? $defaults['format'] ?? Generator::UNDEFINED;
+        $this->enum = $enum ?? $defaults['enum'] ?? Generator::UNDEFINED;
+
         parent::__construct(
-            $parameter,
-            $name,
-            $description,
-            $in,
-            $required,
-            $deprecated,
-            $allowEmptyValue,
-            $ref,
-            $schema,
-            $example,
-            $examples,
-            $content,
-            $style,
-            $explode,
-            $allowReserved,
-            $spaceDelimited,
-            $pipeDelimited,
-            $x,
-            $attachables
+            $parameter ?? $defaults['parameter'] ?? null,
+            $name ?? $defaults['name'] ?? null,
+            $description ?? $defaults['description'] ?? null,
+            $in ?? $defaults['in'] ?? null,
+            $required ?? $defaults['required'] ?? null,
+            $deprecated ?? $defaults['deprecated'] ?? null,
+            $allowEmptyValue ?? $defaults['allowEmptyValue'] ?? null,
+            $ref ?? $defaults['ref'] ?? null,
+            $schema ?? $defaults['schema'] ?? null,
+            $example ?? $defaults['example'] ?? Generator::UNDEFINED,
+            $examples ?? $defaults['examples'] ?? null,
+            $content ?? $defaults['content'] ?? null,
+            $style ?? $defaults['style'] ?? null,
+            $explode ?? $defaults['explode'] ?? null,
+            $allowReserved ?? $defaults['allowReserved'] ?? null,
+            $spaceDelimited ?? $defaults['spaceDelimited'] ?? null,
+            $pipeDelimited ?? $defaults['pipeDelimited'] ?? null,
+            $x ?? $defaults['x'] ?? null,
+            $attachables ?? $defaults['attachables'] ?? null,
         );
+    }
+
+    /**
+     * @return array{
+     *     parameter: string,
+     *     name: string,
+     *     description: string,
+     *     in: string,
+     *     required: bool,
+     *     deprecated: bool,
+     *     allowEmptyValue: bool,
+     *     ref: object|string,
+     *     schema: Schema,
+     *     example: mixed,
+     *     examples: array,
+     *     content: JsonContent|array|Attachable|XmlContent,
+     *     style: string,
+     *     explode: bool,
+     *     allowReserved: bool,
+     *     spaceDelimited: array,
+     *     pipeDelimited: array,
+     *     x: array,
+     *     attachables: array,
+     *     format: string,
+     *     enum: class-string|array,
+     * }
+     */
+    public function defaults(): array
+    {
+        return [];
     }
 }
