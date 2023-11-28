7ee4b3a8584d91d84548454efdf679281fd67337
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-11-28 12:30:10 -0500

    exclude Param from default analyser

--- src/Attribute/Param.php
+++ src/Attribute/Param.php
@@ -2,9 +2,13 @@
 
 namespace OpenSolid\OpenApiBundle\Attribute;
 
-use OpenApi\Attributes\QueryParameter;
+use OpenApi\Annotations\Parameter;
+use OpenApi\Attributes\ParameterTrait;
 
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
-class Param extends QueryParameter
+class Param extends Parameter
 {
+    use ParameterTrait;
+
+    public $in = 'query';
 }
