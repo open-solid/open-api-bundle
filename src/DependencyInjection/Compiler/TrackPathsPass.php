c25d34e680539cc7b0461dd3242e0757aa5aeed5
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-12-02 17:28:00 -0500

    Tracking changes for configured paths

---
+++ src/DependencyInjection/Compiler/TrackPathsPass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OpenSolid\OpenApiBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TrackPathsPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $paths = $container->getParameter('openapi_paths');
+
+        foreach ($paths as $path) {
+            if (!$container->fileExists($path)) {
+                throw new \RuntimeException(sprintf('OpenAPI path "%s" not found.', $path));
+            }
+        }
+    }
+}
--- src/OpenApiBundle.php
+++ src/OpenApiBundle.php
@@ -3,8 +3,10 @@
 namespace OpenSolid\OpenApiBundle;
 
 use OpenSolid\OpenApiBundle\DependencyInjection\Compiler\SerializerMappingPass;
+use OpenSolid\OpenApiBundle\DependencyInjection\Compiler\TrackPathsPass;
 use OpenSolid\OpenApiBundle\DependencyInjection\Compiler\ValidatorMappingPass;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -23,6 +25,7 @@ class OpenApiBundle extends AbstractBundle
     {
         $container->addCompilerPass(new ValidatorMappingPass());
         $container->addCompilerPass(new SerializerMappingPass());
+        $container->addCompilerPass(new TrackPathsPass(), PassConfig::TYPE_AFTER_REMOVING);
     }
 
     public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
